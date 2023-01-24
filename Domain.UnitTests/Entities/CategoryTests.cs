6fe2ce171ee8efbea56fbc439186ee5609b17166
Narisoval <bod13773@gmail.com>
2023-01-24 19:13:32 +0200

    Add test for Category entity

---
+++ Domain.UnitTests/Entities/CategoryTests.cs
@@ -0,0 +1,34 @@
+using Domain.Entities;
+
+namespace Domain.UnitTests.Entities;
+
+public class CategoryTests
+{
+    [Theory]
+    [InlineData("Shoes")]
+    [InlineData("Clothes")]
+    [InlineData("Electronics")]
+    public void Should_CreateCategory_When_NameIsProvided(string name)
+    {
+        //Arrange
+        var id = Guid.NewGuid();
+        
+        //Act
+        var category = new Category(id, name);
+        
+        //Assert
+        Assert.Equal(id, category.Id);
+        Assert.Equal(name, category.Name);
+    }
+
+    [Fact]
+    public void Should_ThrowArgumentNullException_When_NameIsNotProvided()
+    {
+        //Arrange
+        var id = Guid.NewGuid();
+        string name = null;
+        
+        //Act & Assert
+        Assert.Throws<ArgumentNullException>(() => new Category(id, name));
+    }
+}
