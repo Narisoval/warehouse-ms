a79a248407628ce2982aab4e15a716e43bb9c5f3
Narisoval <bod13773@gmail.com>
2023-01-26 01:55:26 +0200

    Change entity base class so two entities are equal, when their ids are equal. Cover it with tests

---
+++ Domain.UnitTests/Foundation/EntityTests.cs
@@ -0,0 +1,81 @@
+using Domain.Primitives;
+using FluentAssertions;
+
+namespace Domain.UnitTests.Foundation;
+
+public class EntityTests
+{
+    [Fact]
+    public void Should_ReturnTrue_When_CompareEntitiesWithSameIds()
+    {
+        //Arrange
+        var id = Guid.NewGuid();
+        var entityA = new TestEntity(id);
+        var entityB = new TestEntity(id);
+
+        //Act
+        var areEntitiesEqual = entityA == entityB;
+        var areEntitiesEqualEqualsMethod = entityA.Equals(entityB);
+        
+        //Assert
+        areEntitiesEqual.Should().BeTrue();
+        areEntitiesEqualEqualsMethod.Should().BeTrue();
+    }
+    
+
+    [Fact]
+    public void Should_ReturnFalse_When_ComparingEntitiesWithDifferentIds()
+    {
+        //Arrange
+        var entityA = new TestEntity(Guid.NewGuid());
+        var entityB = new TestEntity(Guid.NewGuid());
+
+        //Act
+        var areEntitiesEqual = entityA == entityB;
+        var areEntitiesEqualEqualsMethod = entityA.Equals(entityB);
+        
+        var areEntitiesNotEqual = entityA != entityB;
+        
+        //Assert
+        areEntitiesEqual.Should().BeFalse();
+        areEntitiesEqualEqualsMethod.Should().BeFalse();
+        areEntitiesNotEqual.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Should_ReturnFalse_When_ComparingWithNull()
+    {
+        //Arrange
+        var entityA = new TestEntity(Guid.NewGuid());
+        TestEntity? entityB = null; 
+
+        //Act
+        var areEntitiesEqual = entityA == entityB;
+        var areEntitiesEqualEqualsMethod = entityA.Equals(entityB);
+        
+        var areEntitiesNotEqual = entityA != entityB;
+        
+        //Assert
+        areEntitiesEqual.Should().BeFalse();
+        areEntitiesEqualEqualsMethod.Should().BeFalse();
+        areEntitiesNotEqual.Should().BeTrue();
+    }
+    
+    [Fact]
+    public void Should_ThrowException_When_CreatingEntityWithNullId()
+    {
+        //Arrange
+        Guid? id = null;
+        
+        //Act & Assert
+        Assert.Throws<ArgumentNullException>(() => new TestEntity(id));
+    }
+
+    class TestEntity : Entity
+    {
+        public TestEntity(Guid? id) : base(id)
+        {
+            ;
+        }
+    } 
+}
--- Domain/Primitives/Entity.cs
+++ Domain/Primitives/Entity.cs
@@ -12,43 +12,33 @@ public abstract class Entity : IEquatable<Entity>
     }
     public Guid Id { get; }
 
-    public static bool operator ==(Entity? first, Entity? second)
-    {
-        return first is not null && second is not null && first.Equals(second);
-    }
-
-    public static bool operator !=(Entity? first, Entity? second)
-    {
-        return !(first == second);
-    }
-
     public bool Equals(Entity? other)
     {
-        if (other == null)
-            return false;
-        
-        if (other.GetType() == GetType())
-            return false;
-        
-        return other.Id == Id;
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Id.Equals(other.Id);
     }
 
     public override bool Equals(object? obj)
     {
-        if (obj == null)
-            return false;
-
-        if (obj.GetType() != GetType())
-            return false;
-
-        if (obj is not Entity entity)
-            return false;
-        
-        return entity.Id == Id;
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((Entity)obj);
     }
 
     public override int GetHashCode()
     {
-        return Id.GetHashCode() ;
+        return Id.GetHashCode();
+    }
+
+    public static bool operator ==(Entity? left, Entity? right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(Entity? left, Entity? right)
+    {
+        return !Equals(left, right);
     }
 }
