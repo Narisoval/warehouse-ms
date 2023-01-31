4fc7cfa65ef7b5dff95601ea8a0e4bdede80e15b
Narisoval <bod13773@gmail.com>
2023-01-31 21:16:59 +0200

    Add connection string to appsettings.json. Add persistence to app.

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,12 @@
+using Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddPersistence(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
