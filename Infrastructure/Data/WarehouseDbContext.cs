d3a450cf19557e4df3b0fbe33cfc03feead2f618
Narisoval <bod13773@gmail.com>
2023-01-31 21:15:16 +0200

    Add Db context.

---
+++ Infrastructure/Data/WarehouseDbContext.cs
@@ -0,0 +1,22 @@
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class WarehouseDbContext : DbContext
+{
+    public WarehouseDbContext(DbContextOptions<WarehouseDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Provider> Providers { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Brand> Brands { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(WarehouseDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
+}
