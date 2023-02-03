5b67cd2ae404a5c54c22d29830a364e84552743e
Narisoval <bod13773@gmail.com>
2023-02-03 17:08:44 +0200

    Change product fixture so every product has unique ID
    Change product fixture so every time GetTestProduct is call id is different

--- Domain.UnitTests/Fixtures/ProductsFixture.cs
+++ Domain.UnitTests/Fixtures/ProductsFixture.cs
@@ -5,65 +5,75 @@ namespace Domain.UnitTests.Fixtures;
 
 public static class ProductsFixture
 {
-    private static readonly Guid TestId = Guid.NewGuid();
     private static readonly ProductName TestName = ProductName.From("Kingston 3200 TB hard drive");
     private static readonly Quantity TestQuantity = Quantity.From(300);
     private static readonly Price TestPrice = Price.From(500.99M);
-    private static readonly IList<ProductImage> TestProductImages = ProductImagesFixture.GetTestProductImages();
     private static readonly ProductDescription TestProductDescription = ProductDescription.From(new string('1', 50));
     private static readonly bool TestIsActive = true;
     private static readonly Sale TestSale = Sale.From(0);
     private static readonly Provider TestProvider = ProvidersFixture.GetTestProvider();
-    private static readonly Brand TestBrand = BrandsFixture.GetTestBrand();
     private static readonly Category TestCategory = CategoriesFixture.GetTestCategory();
     public static Product GetTestProduct()
     {
-        return Product.Create(TestId,
+        Guid testId = Guid.NewGuid();
+        IList<ProductImage> testProductImages = ProductImagesFixture.GetTestProductImages();
+        Brand testBrand = BrandsFixture.GetTestBrand();
+        return Product.Create(testId,
             TestName,
             TestQuantity,
             TestPrice,
-            TestProductImages,
+            testProductImages,
             TestProductDescription,
             TestIsActive,
             TestSale,
             TestProvider,
-            TestBrand,
+            testBrand,
             TestCategory);
     }
     
     public static Product GetTestProduct(int intQuantity)
     {
-        return Product.Create(TestId,
+        Guid testId = Guid.NewGuid();
+        IList<ProductImage> testProductImages = ProductImagesFixture.GetTestProductImages();
+        Brand testBrand = BrandsFixture.GetTestBrand();
+        return Product.Create(testId,
             TestName,
             Quantity.From(intQuantity),
             TestPrice,
-            TestProductImages,
+            testProductImages,
             TestProductDescription,
             TestIsActive,
             TestSale,
             TestProvider,
-            TestBrand,
+            testBrand,
             TestCategory);
     }
     
     public static Product GetTestProduct(bool isActiveParam)
     {
-        return Product.Create(TestId,
+        Guid testId = Guid.NewGuid();
+        IList<ProductImage> testProductImages = ProductImagesFixture.GetTestProductImages();
+        Brand testBrand = BrandsFixture.GetTestBrand();
+        
+        return Product.Create(testId,
             TestName,
             TestQuantity,
             TestPrice,
-            TestProductImages,
+            testProductImages,
             TestProductDescription,
             isActiveParam,
             TestSale,
             TestProvider,
-            TestBrand,
+            testBrand,
             TestCategory);
     }
     
     public static Product GetTestProduct(IList<ProductImage> productImages)
     {
-        return Product.Create(TestId,
+        Guid testId = Guid.NewGuid();
+        Brand testBrand = BrandsFixture.GetTestBrand();
+        
+        return Product.Create(testId,
             TestName,
             TestQuantity,
             TestPrice,
@@ -72,7 +82,7 @@ public static class ProductsFixture
             TestIsActive,
             TestSale,
             TestProvider,
-            TestBrand,
+            testBrand,
             TestCategory);
     }
 }
