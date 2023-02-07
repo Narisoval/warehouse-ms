500f67f28b4f3346fce633efe279500f91618fe8
Narisoval <bod13773@gmail.com>
2023-02-07 13:59:20 +0200

    Add IsMain property to ProductImage

--- Domain/Entities/ProductImage.cs
+++ Domain/Entities/ProductImage.cs
@@ -7,16 +7,20 @@ public class ProductImage : Entity
 {
     public Image Image { get; internal set; }
 
+    public bool IsMain { get; set; }
+    
     public Product? Product { get; set; }
+    
     public Guid ProductId { get; set; }
 
-    private ProductImage(Guid id, Image image) : base(id)
+    private ProductImage(Guid id, Image image, bool isMain) : base(id)
     {
         Image = image ?? throw new ArgumentNullException(nameof(image));
+        IsMain = isMain;
     }
 
-    public static ProductImage Create(Guid id, Image image)
+    public static ProductImage Create(Guid id, Image image, bool isMain)
     {
-        return new ProductImage(id, image);
+        return new ProductImage(id, image,isMain);
     }
 }
