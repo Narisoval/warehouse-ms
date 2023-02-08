ed258b0ec7f7ecc3daaf0316f6d80303112f548b
Narisoval <bod13773@gmail.com>
2023-02-08 22:29:40 +0200

    Add DTos for all entities

---
+++ API/DTO/BrandDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+
+namespace Warehouse.API.DTO;
+
+public record BrandDto
+{
+    public Guid BrandId { get; init; }
+
+    public string Name { get; init; }
+
+    public string Image { get; init; }
+
+    public string Description { get; set; }
+    
+    public IList<ProductDto>? Products { get; set; }
+}
