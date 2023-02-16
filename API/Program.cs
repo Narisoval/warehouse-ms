8e0b6f7c01c6eddf834e5686f997c57a79dc94ee
Narisoval <bod13773@gmail.com>
2023-02-16 02:52:55 +0200

    Set up controllers in Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -3,10 +3,13 @@ using Infrastructure;
 var builder = WebApplication.CreateBuilder(args);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddControllers();
+builder.Services.AddPersistence(builder.Configuration);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddPersistence(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,4 +19,5 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.MapControllers();
 app.Run();
