55b7c5ddd0c90d27ed1419eb745d0b9a9eae5898
Narisoval <bod13773@gmail.com>
2023-03-05 18:55:17 +0200

    Add new tests for category

--- Domain.UnitTests/Entities/CategoryTests.cs
+++ Domain.UnitTests/Entities/CategoryTests.cs
@@ -6,6 +6,52 @@ namespace Domain.UnitTests.Entities;
 
 public class CategoryTests
 {
+    
+    private readonly CategoryName _categoryName = CategoryName.From("type").Value;
+    
+    [Fact]
+    public void Should_CreateCategory_When_CreatingCategoryWithId()
+    {
+        //Arrange
+        var id = Guid.NewGuid();
+        
+        //Act 
+        var sut = Category.Create(id, _categoryName);
+        
+        //Assert
+        sut.IsSuccess.Should().BeTrue();
+        sut.Value.Id.Should().Be(id);
+        sut.Value.Name.Should().BeEquivalentTo(_categoryName);
+
+    }
+    
+    [Fact]
+    public void Should_CreateCategory_When_CreatingCategoryWithoutId()
+    {
+        //Act 
+        var sut = Category.Create(_categoryName);
+        
+        //Assert
+        sut.IsSuccess.Should().BeTrue();
+        sut.Value.Id.Should().NotBe(Guid.Empty);
+        sut.Value.Name.Should().BeEquivalentTo(_categoryName);
+
+    }
+    
+    [Fact]
+    public void Should_ReturnFailedResult_When_CategoryIsCreatedWithEmptyGuid()
+    {
+        //Arrange 
+        var id = Guid.Empty;
+        
+        //Act
+        var sut = Category.Create(id, _categoryName);
+        
+        //Assert
+        sut.AssertIsFailed(1);
+    }
+    
+    
     [Fact]
     public void Should_ReturnFailedResult_When_ArgumentsAreNull()
     {
@@ -17,22 +63,6 @@ public class CategoryTests
         var categoryResult = Category.Create(id, categoryName);
         
         //Assert
-        categoryResult.IsFailed.Should().BeTrue();
+        categoryResult.AssertIsFailed(1);
     }
-    [Fact]
-    public void Should_CreateCategory_When_ArgumentsAreValid()
-    {
-        //Arrange
-        CategoryName? categoryName = CategoryName.From("type").Value;
-        var id = Guid.NewGuid();
-        
-        //Act 
-        var sut = Category.Create(id, categoryName);
-        
-        //Assert
-        sut.IsSuccess.Should().BeTrue();
-        sut.Value.Name.Should().BeEquivalentTo(categoryName);
-
-    }
-    
 }
