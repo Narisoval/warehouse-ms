639ba6f2bb56bf439a8d18e076362225c38c4e21
Narisoval <bod13773@gmail.com>
2023-03-24 17:09:56 +0200

    Remove unused methods in IRepository interface and its generic implementation

--- Infrastructure/Interfaces/IRepository.cs
+++ Infrastructure/Interfaces/IRepository.cs
@@ -1,4 +1,3 @@
-using System.Linq.Expressions;
 using Domain.Primitives;
 
 namespace Infrastructure.Interfaces;
@@ -6,16 +5,12 @@ namespace Infrastructure.Interfaces;
 public interface IRepository<TEntity> where  TEntity : Entity
 {
    Task<TEntity?> Get(Guid id);
+   
    Task<IEnumerable<TEntity>> GetAll();
-   IEnumerable<TEntity> Find(Expression<Func<TEntity,bool>> predicate);
 
    Task Add(TEntity entity);
-   Task AddRange(IEnumerable<TEntity> entities);
 
    Task<bool> Update(TEntity entity);
-   
-   void Remove(TEntity entity);
-   void RemoveRange(IEnumerable<TEntity> entities);
 
    Task<bool> Remove(Guid id);
 }
--- Infrastructure/Repositories/Repository.cs
+++ Infrastructure/Repositories/Repository.cs
@@ -1,11 +1,12 @@
-using System.Linq.Expressions;
 using Domain.Primitives;
 using Infrastructure.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories;
 
-public abstract class Repository<TEntity,TContext> : IRepository<TEntity> where TEntity : Entity where TContext : DbContext
+public abstract class Repository<TEntity, TContext> : IRepository<TEntity>
+    where TEntity : Entity 
+    where TContext : DbContext
 {
     protected readonly TContext Context;
 
@@ -24,48 +25,28 @@ public abstract class Repository<TEntity,TContext> : IRepository<TEntity> where 
         return await Context.Set<TEntity>().ToListAsync();
     }
 
-    public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
-    {
-        return Context.Set<TEntity>().Where(predicate);
-    }
-
     public async Task Add(TEntity entity)
     {
         await Context.Set<TEntity>().AddAsync(entity);
     }
 
-    public async Task AddRange(IEnumerable<TEntity?> entities)
-    {
-        await Context.Set<TEntity>().AddRangeAsync(entities);
-    }
-
     public async Task<bool> Update(TEntity entity)
     {
         var entityExists = await Context.Set<TEntity>().AnyAsync(e => e.Id == entity.Id);
-        
+
         if (!entityExists)
         {
-            return false; 
+            return false;
         }
-        
+
         Context.Set<TEntity>().Update(entity);
         return true;
-    }
-
-    public void Remove(TEntity entity)
-    {
-        Context.Set<TEntity>().Remove(entity);
-    }
-
-    public void RemoveRange(IEnumerable<TEntity> entities)
-    {
-        Context.Set<TEntity>().RemoveRange(entities);
     }
 
     public async Task<bool> Remove(Guid id)
     {
         var entityFromDb = await Get(id);
-        
+
         if (entityFromDb == null)
             return false;
 
