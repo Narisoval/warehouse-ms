e194a3675d985f2931097458910ac464b9fbe202
Narisoval <bod13773@gmail.com>
2023-03-24 17:11:38 +0200

    Remove unused methods in IProvider interface and its implementation

--- Infrastructure/Interfaces/IProviderRepository.cs
+++ Infrastructure/Interfaces/IProviderRepository.cs
@@ -4,6 +4,4 @@ namespace Infrastructure.Interfaces;
 
 public interface IProviderRepository : IRepository<Provider>
 {
-    Task<Provider?> GetProviderWithProducts(Guid id);
-    Task<IEnumerable<Provider>> GetAllProvidersWithProducts();
 }
--- Infrastructure/Repositories/ProviderRepository.cs
+++ Infrastructure/Repositories/ProviderRepository.cs
@@ -1,7 +1,6 @@
 using Domain.Entities;
 using Infrastructure.Data;
 using Infrastructure.Interfaces;
-using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories;
 
@@ -11,22 +10,5 @@ public sealed class ProviderRepository : Repository<Provider,WarehouseDbContext>
     public ProviderRepository(WarehouseDbContext context) : base(context)
     {
         ;
-    }
-
-    public async Task<Provider?> GetProviderWithProducts(Guid id)
-    {
-        return await Context.Providers
-            .AsNoTracking()
-            .Include(provider => provider.Products)
-            .Where(provider => provider.Id == id)
-            .FirstOrDefaultAsync();
-    }
-
-    public async Task<IEnumerable<Provider>> GetAllProvidersWithProducts()
-    {
-        return await Context.Providers
-            .AsNoTracking()
-            .Include(provider => provider.Products)
-            .ToListAsync();
     }
 }
