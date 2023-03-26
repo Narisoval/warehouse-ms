dce36b585be71d8a3b20b2c228ff790ff1a8e3ba
Narisoval <bod13773@gmail.com>
2023-03-26 17:09:36 +0300

    Add tests for product repository.
    It doesn't really test any behaviour related product images as their uniqueness, etc. That is specifically done that way because the image upload is still not implemented and thing could change when it will be.

---
+++ Infrastructure.IntegrationTests/RepositoryTests/ProductRepositoryTests.cs
@@ -0,0 +1,203 @@
+using Domain.Entities;
+using Domain.ValueObjects;
+using FluentAssertions;
+using Infrastructure.IntegrationTests.Helpers;
+using Infrastructure.IntegrationTests.Helpers.Fixtures;
+using Infrastructure.Interfaces;
+
+namespace Infrastructure.IntegrationTests.RepositoryTests;
+
+public class ProductRepositoryTests : RepositoryTestsBase<Product,IProductRepository>
+{
+    public ProductRepositoryTests(DatabaseFixture fixture) : base(fixture)
+    {
+    }
+
+    [Fact]
+    public async Task Should_GetProductWithAllRelatedEntities_When_GettingAllProducts()
+    {
+        //Act
+        var allProducts = await Repository.GetAll();
+        
+        //Assert
+        foreach (var product in allProducts)
+        {
+            AssertProductHasCorrectNavigationalProperties(product);
+        }
+    }
+
+    private void AssertProductHasCorrectNavigationalProperties(Product? product)
+    {
+        product.Should().NotBeNull();
+        
+        product!.Brand.Should().NotBeNull();
+        product.Category.Should().NotBeNull();
+        product.Provider.Should().NotBeNull();
+    }
+    
+    [Fact]
+    public async Task Should_GetProductWithAllRelatedEntities_When_GettingProductById()
+    {
+        //Arrange
+        var seededProductId = GetRandomSeededEntity().Id;
+        
+        //Act
+        var productFromDb = await Repository.Get(seededProductId);
+        
+        //Assert
+        AssertProductHasCorrectNavigationalProperties(productFromDb);
+    }
+    
+    [Fact]
+    public async Task Should_NotAddProduct_When_ForeignKeysAreWrong()
+    {
+        //Assert 
+        var productFromDb = GetRandomSeededEntity();
+        
+        var productWithWrongProviderId = ProductHelpers.GetNewProductWithChangedForeignKeys(productFromDb, providerId: Guid.NewGuid());
+        
+        var productWithWrongBrandId = ProductHelpers.GetNewProductWithChangedForeignKeys(productFromDb, brandId: Guid.NewGuid());
+        
+        var productWithWrongCategoryId = ProductHelpers.GetNewProductWithChangedForeignKeys(productFromDb, categoryId: Guid.NewGuid());
+         
+        //Act
+        await Repository.Add(productWithWrongProviderId);
+        await Repository.Add(productWithWrongBrandId);
+        await Repository.Add(productWithWrongCategoryId);
+
+        var productFromDbWithWrongProviderId = await Repository.Get(productWithWrongProviderId.Id);
+        var productFromDbWithWrongBrandId = await Repository.Get(productWithWrongBrandId.Id);
+        var productFromDbWithWrongCategoryId = await Repository.Get(productWithWrongCategoryId.Id);
+        
+        //Assert
+        productFromDbWithWrongProviderId.Should().BeNull();
+        productFromDbWithWrongBrandId.Should().BeNull();
+        productFromDbWithWrongCategoryId.Should().BeNull();
+    }
+    
+    public static IEnumerable<object[]> TestData =>
+        new List<object[]>
+        {
+            new object[] { null, null, Guid.NewGuid(), 1 },
+            new object[] { null, Guid.NewGuid(), Guid.NewGuid(), 2 },
+            new object[] { Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), 3 }
+        };
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public async Task Should_ReturnRightAmountOfErrors_When_AddingCategoryWithWrongForeignKeys
+    (Guid? providerId, Guid? brandId, Guid? categoryId, int expectedErrorCount)
+    {
+        // Arrange
+        var productFromDb = GetRandomSeededEntity();
+        var productWrongForeignKeys = ProductHelpers.GetNewProductWithChangedForeignKeys
+            (productFromDb, providerId, brandId, categoryId);
+
+        // Act
+        var result = await Repository.Add(productWrongForeignKeys);
+
+        // Assert
+        result.IsFailed.Should().BeTrue();
+        result.Errors.Count.Should().Be(expectedErrorCount);
+    }
+    
+    [Fact]
+    public override async Task Should_UpdateEntity_When_Called()
+    {
+        // Arrange
+        var product = GetRandomSeededEntity();
+
+        var updatedProduct = Product.Create(
+            id: product.Id,
+            productName: ProductName.From("Updated Product").Value,
+            quantity: Quantity.From(50).Value,
+            fullPrice: Price.From(25).Value,
+            mainImage: Image.From("https://updatedimage.png").Value,
+            images: new List<ProductImage>
+            {
+                ProductImage.Create(Image.From("https://newimage.png").Value).Value
+            },
+            productDescription: ProductDescription.From("Updated description of a marvelous product").Value,
+            isActive: false,
+            sale: Sale.From(10).Value,
+            providerId: product.ProviderId,
+            brandId: product.BrandId,
+            categoryId: product.CategoryId
+        ).Value;
+        
+        // Act
+        var result = await Repository.Update(updatedProduct);
+        await UnitOfWork.Complete();
+        var fetchedProduct = await Repository.Get(product.Id);
+        
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().BeTrue();
+        fetchedProduct.Should().BeEquivalentTo(updatedProduct);
+    }
+    
+    [Fact]
+    public override async Task Should_ReturnFalse_When_UpdatingNonExistingEntity()
+    {
+        // Arrange
+        var existingProduct = GetRandomSeededEntity();
+        var nonExistingProduct = Product.Create(
+            Guid.NewGuid(),
+            existingProduct.Name,
+            existingProduct.Quantity,
+            existingProduct.FullPrice,
+            existingProduct.MainImage,
+            existingProduct.Images,
+            existingProduct.Description,
+            existingProduct.IsActive,
+            existingProduct.Sale,
+            existingProduct.ProviderId,
+            existingProduct.BrandId,
+            existingProduct.CategoryId
+        ).Value;
+
+        // Act
+        var result = await Repository.Update(nonExistingProduct);
+        await UnitOfWork.Complete();
+
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().BeFalse();
+    }
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public async Task Should_ReturnRightAmountOfErrors_When_UpdatingProductWithWrongForeignKeys
+    (Guid? providerId, Guid? brandId, Guid? categoryId, int expectedErrorCount)
+    {
+        // Arrange
+        var productFromDb = GetRandomSeededEntity();
+        var productWrongForeignKeys = ProductHelpers.GetNewProductWithChangedForeignKeys
+            (productFromDb, providerId, brandId, categoryId);
+
+        // Act
+        var result = await Repository.Update(productWrongForeignKeys);
+
+        // Assert
+        result.IsFailed.Should().BeTrue();
+        result.Errors.Count.Should().Be(expectedErrorCount);
+    }
+    
+    [Fact]
+    public override async Task Should_NotChangeAnything_When_UpdatingEntityWithNoChanges()
+    {
+        // Arrange
+        var product = GetRandomSeededEntity();
+
+        // Act
+        var result = await Repository.Update(product);
+        await UnitOfWork.Complete();
+        
+        var fetchedProduct = await Repository.Get(product.Id);
+         
+        // Assert
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().BeTrue();
+        fetchedProduct.Should().Be(product);
+    }
+}
